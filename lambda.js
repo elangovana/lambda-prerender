339b09c97ecf2cf64f394b10fdfdbf1b16ad7484
aeg <aeg@amazon.com>
2018-05-28 14:41:21 +1000

    fix headless chrome version

--- lambda.js
+++ lambda.js
@@ -1,6 +1,12 @@
 'use strict'
 const prerender = require('prerender');
-const prerenderServer = prerender({chromeLocation: "./node_modules/@serverless-chrome/lambda/dist/headless-chromium", 'port':'5000', chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]});
+const prerenderServer = prerender({chromeLocation: "./node_modules/@serverless-chrome/lambda/dist/headless-chromium", 'port':'5000', chromeFlags: ['--headless', ' --crash-dumps-dir=/tmp', '--no-sandbox', '--disable-gpu', '--window-size=1280,1696',
+                   '--hide-scrollbars'
+            , '--homedir=/tmp'
+            , '--single-process'
+            , '--data-path=/tmp/data-path'
+            , '--disk-cache-dir=/tmp/cache-dir'
+            ,'--ignore-certificate-errors','--remote-debugging-port=9222' ]});
 const awsServerlessExpress = require('aws-serverless-express')
 const binaryMimeTypes = [
 	'application/octet-stream',
@@ -9,20 +15,60 @@ const binaryMimeTypes = [
 	'font/otf',
 	'image/jpeg',
 	'image/png',
-	'image/svg+xml'
+	'image/svg+xml',
+	"text/html"
 ]
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+prerenderServer.start();
 
 //This is regular aws serverless express
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
-exports.handler = (event, context) => {
+exports.handler = (event, context, callback) => {
 	app.disable('x-powered-by');
 	app.use(compression());
 	app.get('*', prerenderServer.onRequest);
 	app.post('*', bodyParser.json({ type: () => true }), prerenderServer.onRequest);
-	prerenderServer.start()
-	return awsServerlessExpress.proxy(server, event, context)
+	console.log("About to start prerenderServer .. aka Headless chrome");
+
+	console.log("Started prerenderServer ..");
+	console.log("Testing if the prerender is working..");
+	const url = event["url"];
+
+	const http= require('http');
+
+  http.get('http://localhost:5000/render?url='+url, (resp) => {
+	  let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+	    data += chunk;
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+	    console.log(data);
+			console.log("Test Complete success.." );
+			const successResponse = {
+	        statusCode: 200, // "DNS resolution, TCP level errors, or actual HTTP parse errors" - https://nodejs.org/api/http.html#http_http_request_options_callback
+	        body: data,
+	        headers: {}
+	    }
+			context.succeeded( successResponse);
+	  });
+
+	})
+	.on("error", (err) => {
+  	console.log( "Error: " + err.message);
+		console.log("Test Complete with error.." );
+		callback("Error: " + err.message, data)
+		const sucessFailed = {
+				statusCode: 502, // "DNS resolution, TCP level errors, or actual HTTP parse errors" - https://nodejs.org/api/http.html#http_http_request_options_callback
+				body: err.message,
+				headers: {}
+		}
+		context.succeeded( sucessFailed);
+	});
+
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const prerender = require('prerender');
-const prerenderServer = prerender({'port':'5000'});
+const prerenderServer = prerender({ 'port':'5000', chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--hide-scrollbars','--remote-debugging-port=9222' ]});
 const express = require('express');
 const app = express();
 const compression = require('compression');
@@ -11,5 +11,28 @@ app.get('*', prerenderServer.onRequest);
 app.post('*', bodyParser.json({ type: () => true }), prerenderServer.onRequest);
 
 app.listen(3000, ()=> {console.log("Listening on 3000");})
+prerenderServer.start();
+submitSampleRequest();
 
-prerenderServer.start()
+function submitSampleRequest(){
+	JSON.stringify(app);
+	const http= require('http');
+
+  http.get('http://localhost:5000/render?url=https://ahfarmer.github.io/calculator/', (resp) => {
+	  let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+	    data += chunk;
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+	    console.log(data);
+	  });
+
+	})
+	.on("error", (err) => {
+  	console.log("Error: " + err.message);
+	});
+}
